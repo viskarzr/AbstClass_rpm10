53266b60cde902cdc085d7ded80195b2bc0e57dc
ИСП-31 <-31@LAB17-05>
2025-11-20 09:59:47 +0300

    Добавьте файлы проекта.

---
+++ RPM10/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RPM10
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            cbAnimals.Items.Add("Млекопитающие");
+            cbAnimals.Items.Add("Парнокопытные");
+            cbAnimals.Items.Add("Птицы");
+        }
+        private List<Animal> listAmimals = new List<Animal>();
+        private Animal selectedAnimals = null;
+
+        private void UpdateAnimalsList()
+        {
+            lbAnimal.ItemsSource = null;
+            lbAnimal.ItemsSource = listAmimals;
+        }
+
+        private void ClearFields()
+        {
+            tbId.Clear();
+            tbName.Clear();
+            tbAge.Clear();
+            tbWeight.Clear();
+            cbAnimals.SelectedIndex = 0;
+        }
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            bool f1, f2, f3;
+            f1 = int.TryParse(tbAge.Text, out int age);
+            f2 = double.TryParse(tbWeight.Text, out double weight);
+            f3 = int.TryParse(tbId.Text, out int id);
+            if (f1 == f2 == f3 == true && age > 0 && age < 100 && weight > 0)
+            {
+                Animal Animals = null;
+                switch (cbAnimals.Text)
+                {
+                    case "Млекопитающие":
+                        Animals = new Mammals();
+                        break;
+                    case "Парнокопытные":
+                        Animals = new Artiodactyls();
+                        break;
+                    case "Птицы":
+                        Animals = new Birds();
+                        break;
+                }
+                if (Animals != null)
+                {
+                    Animals.UpdateAnimals(id,tbName.Text, age, weight);
+                    UpdateAnimalsList();
+                    ClearFields();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Проверьте корректность ввода!\n Пометка:\nВозраст должен быть в диапозоне от 0 до 100, a вес больше 0");
+            }
+        }
+
+        private void btnChange_Click(object sender, RoutedEventArgs e)
+        {
+            bool f1, f2, f3;
+            f1 = int.TryParse(tbAge.Text, out int age);
+            f2 = double.TryParse(tbWeight.Text, out double weight);
+            f3 = int.TryParse(tbId.Text, out int id);
+            if (f1 == f2 == f3 == true && age > 0 && age < 100 && weight > 0)
+            {
+                if (selectedAnimals != null)
+                {
+                    try
+                    {
+                        selectedAnimals.UpdateAnimals(id, tbName.Text, age, weight);
+                        UpdateAnimalsList();
+                        ClearFields();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Ошибка: {ex.Message}");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Проверьте корректность ввода или выберите животное для изменения!\n Пометка:\nВозраст должен быть в диапозоне от 0 до 100, a вес больше 0");
+            }
+            
+        }
+
+        private void btnDel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAbout_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Практическая работа №10 - Работа с абстрактными классами. Вариант 5. Создать Класс, содержащий колекции объектов - Зоопарк. Базовый абстрактный класс - Животное. Производные классы - млекопитающие, парнокопытные, птицы.\nВыполнила:\nстудентка ИСП-31\nКирюшова В.");
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ RPM10/Zoo.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPM10
+{
+    public abstract class Animal
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; } 
+        public double Weight { get; set; }
+
+        public abstract string Animals { get; }
+        public abstract void UpdateAnimals(int id,string name, int age, double weight);
+
+        public override string ToString()
+        {
+            return $"{Animals}:{ID} {Name}, возраст: {Age}, вес: {Weight}.";
+        }
+    }
+
+    public class Mammals : Animal
+    {
+        public override string Animals
+        {
+            get { return "Mлекопитающее"; }
+        }
+
+        public override void UpdateAnimals(int id, string name, int age, double weight)
+        {
+            ID = id;
+            Name = name;
+            Age = age;
+            Weight = weight;
+        }
+
+    }
+
+    public class Artiodactyls : Animal
+    {
+        public override string Animals
+        {
+            get { return "Парнокопытное"; }
+        }
+
+        public override void UpdateAnimals(int id, string name, int age, double weight)
+        {
+            ID = id;
+            Name = name;
+            Age = age;
+            Weight = weight;
+        }
+    }
+
+    public class Birds: Animal
+    {
+        public override string Animals
+        {
+            get { return "Птица"; }
+        }
+
+        public override void UpdateAnimals(int id, string name, int age, double weight)
+        {
+            ID = id;
+            Name = name;
+            Age = age;
+            Weight = weight;
+        }
+
+    }
+
+}
+    
+
